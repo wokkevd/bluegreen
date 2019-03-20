a8c532d40d2e8e4d222443813f522751b38e4923
Wouter <47855464+wokkevd@users.noreply.github.com>
2019-03-20 14:43:58 +0100

    Less colors

--- image.php
+++ image.php
@@ -5,16 +5,17 @@ $white = imagecolorallocate($im, 255, 255, 255);
 // Line reserved to trigger the webhook in the lab. 
 $deployment = getenv("COLOR");
 
-if ( $deployment == 'blue') {
+/*if ( $deployment == 'blue') {
   $color = imagecolorallocate($im, 0, 0, 255);
 } elseif ($deployment == 'green')  {
   $color = imagecolorallocate($im, 0, 255, 0);
 } else {
   $color = imagecolorallocate($im, 0, 0, 255);
-}
+}*/
+$color = imagecolorallocate($im, 255,0,0);
 
 // Draw a filled rectangle
-imagefilledrectangle($im, 0, 0, 200, 200, $color);
+imagefilledrectangle($im, 0, 0, 199, 199, $color);
 
 // Save the image
 header('Content-Type: image/png');
